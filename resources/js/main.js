18dc90bfa1b45bbc30964fc08204bf68d74be3b1
Baboo81 <magicbaboo@gmail.com>
2026-01-22 15:40:05 +0100

    section : offers

--- resources/js/main.js
+++ resources/js/main.js
@@ -13,3 +13,39 @@ window.addEventListener('scroll', function() {
         nav.classList.remove('sticky');
     }
 });
+
+// ----------------------------
+// Zone de progression
+// ----------------------------
+window.addEventListener('scroll', () => {
+  const circle = document.getElementById('scroll-indicator');
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+  const color = 'green';
+  const arrow = document.querySelector('.scroll-arrow');
+
+  circle.style.background = `conic-gradient(${color} ${scrollPercent}%, transparent 0%)`;
+
+  //Mise à jour de la direction de la flèche :
+  if (scrollPercent >= 98) {
+    arrow.innerHTML = '&#8593;'; // flèche vers le haut ↑
+  } else {
+    arrow.innerHTML = '&#x2193;'; // flèche vers le bas ↓
+  }
+
+});
+
+//---------------------------
+// Cards-popup
+//---------------------------
+function showPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    if (popup) popup.style.display = "flex";
+}
+
+function closePopup(popupId) {
+    const popup = document.getElementById(popupId);
+    if (popup) popup.style.display = "none";
+}
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,6 +28,12 @@
     </section>
     {{-- Section : Banner END --}}
 
+    <!-- SVG WAVE -->
+    <svg class="waveWeb" viewBox="0 0 1440 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true" role="img">
+        <path d="M0,32 C200,120 400,0 720,48 C1040,96 1240,0 1440,48 L1440 120 L0 120 Z" fill="#FFFFF0"></path>
+    </svg>
+
     {{-- Section : Purposes --}}
     <section class="purposes">
         <div class="containter">
@@ -56,4 +62,57 @@
         </div>
     </section>
     {{-- Section : Purposes END --}}
+
+    {{-- Section : Offers  --}}
+    <section class="offers">
+        <div class="container">
+            <div class="row">
+                <div class="my-5">
+                    <h1 class="text-center titleH1">
+                        {{ $homeData['offers']['main_Title'] ?? '' }}
+                    </h1>
+                </div>
+                @foreach ($homeData['offers']['cards'] as $index => $offer)
+                    <div class="col-xl-4 col-xxl-4 col-lg-12 col-md-12 col-sm-12 col-xs-12 my-5">
+                        <div class="card rounded-5">
+                            <img src="{{ asset($offer['img']) ?? '' }}" class="card-img-top rounded-top-5"
+                                alt="{{ $offer['alt'] }}">
+                            <div class="card-body d-flex flex-column text-center">
+                                <h5 class="card-title text-center fs-3 my-3">{{ $offer['title'] }}</h5>
+                                <h6 class="fontBolded fs-5 my-3">{{ $offer['price'] }}</h6>
+                                <p class="card-text">{{ $offer['text'] }}</p>
+                                <div class="button-container mt-auto justify-content-center">
+                                    <button onclick="showPopup('popup{{ $index + 1 }}')" class="my-3">À qui s'adresse
+                                        cette offre ?</button>
+                                </div>
+                            </div>
+
+                            {{-- Popup --}}
+                            <div class="popup-container" id="popup{{ $index + 1 }}">
+                                <div class="popup">
+                                    <h4 class="fs-3 my-3">{{ $offer['popup']['title'] }}</h4>
+                                    <img src="{{ asset($offer['popup']['img']) ?? '' }}" alt="">
+                                    <p class="text-start my-3">{{ $offer['popup']['intro'] }}</p>
+                                    <ul class="text-start">
+                                        @foreach ($offer['popup']['items'] as $item)
+                                            <li class="my-2">&nbsp;{{ $item }}</li>
+                                        @endforeach
+                                    </ul>
+                                    @if (!empty($offer['popup']['outro']))
+                                        <p class="pPopUp fs-6 my-3">{{ $offer['popup']['outro'] }}</p>
+                                    @endif
+                                    <div class="button-container justify-content-center">
+                                        <button onclick="closePopup('popup{{ $index + 1 }}')"
+                                            class="my-3">Fermer</button>
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- Popup END --}}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+    {{-- Section : Offers  END --}}
 @endsection
