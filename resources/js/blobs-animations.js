cf76a33379c205305588e498013e84ebb18a4590
Baboo81 <magicbaboo@gmail.com>
2026-01-26 14:09:22 +0100

    animation : blob

---
+++ resources/js/blobs-animations.js
@@ -0,0 +1,22 @@
+import gsap from "gsap";
+
+//Blob 1 :
+gsap.to("#blob1", {
+    x: 1900,            // déplacement horizontal
+    scale: 1.2,         // pulse
+    opacity: 0.2,       // opacité
+    duration: 19.5,
+    repeat: -1,
+    yoyo: true,         // droite-gauche
+    transformOrigin: "50% 50%",
+    ease: "power1.inOut"
+});
+
+gsap.to("#blob1", {
+    rotate: 360,
+    duration: 10,
+    repeat: -1,
+    ease: "linear",
+});
+
+
