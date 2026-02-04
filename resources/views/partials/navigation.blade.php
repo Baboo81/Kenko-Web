262c2e0872e7627077c717e765f0994a851a6356
Baboo81 <magicbaboo@gmail.com>
2026-02-04 10:48:53 +0100

    design

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,7 +1,9 @@
 <header>
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">KENKO-WEB</a>
+            <a class="navbar-brand" href="{{ route('home') }}">
+                <img src="{{ asset('assets/img/logo/logoKenkoWeb.svg') }}" alt="Logo du site Kenko-Web" height="95">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
